adfbc872a49be48a63866350582bc470576aa4e1
Mickael Bossa <mickael.bossa@outlook.com>
2022-12-17 11:27:58 +0100

    Add date and calendar functionnality to search input

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,36 @@ import {
   GlobeAltIcon,
   MagnifyingGlassIcon,
   UserCircleIcon,
+  UserIcon,
 } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker, RangeKeyDict } from 'react-date-range';
 
 export default function Header() {
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [startDate, setStartDate] = useState<Date | undefined>(new Date());
+  const [endDate, setEndDate] = useState<Date | undefined>(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
+  const [nbrOfGuests, setNbrOfGuests] = useState<string>('1');
+
+  const handleSelect = (ranges: RangeKeyDict) => {
+    console.log(ranges);
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput('');
+  };
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 shadow-md bg-white p-5 md:px-10'>
       <div className='relative flex items-center h-10 cursor-pointer my-auto'>
@@ -20,6 +47,8 @@ export default function Header() {
 
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type='text'
           placeholder='Start your search'
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
@@ -28,13 +57,45 @@ export default function Header() {
       </div>
 
       <div className='flex items-center space-x-4 justify-end text-gray-500'>
-        <p className='hidden md:inline text-center cursor-pointer'>Become a host</p>
+        <p className='hidden md:inline text-center cursor-pointer'>
+          Become a host
+        </p>
         <GlobeAltIcon className='h-6 cursor-pointer' />
         <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
           <Bars3Icon className='h-6' />
           <UserCircleIcon className='h-6' />
         </div>
       </div>
+
+      {searchInput && (
+        <div className='flex flex-col col-span-3 mx-auto mt-1'>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+          <div className='flex items-center border-b mb-4'>
+            <h2 className='text-2xl flex-grow font-semibold'>
+              Number of Guests
+            </h2>
+            <UserIcon className='h-5' />
+            <input
+              value={nbrOfGuests}
+              onChange={(e) => setNbrOfGuests(e.target.value)}
+              type='number'
+              min={'1'}
+              className='w-12 pl-2 text-lg outline-none text-red-400'
+            />
+          </div>
+          <div className='flex'>
+            <button onClick={resetInput} className='flex-grow text-gray-500'>
+              Cancel
+            </button>
+            <button className='flex-grow text-red-400'>Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
